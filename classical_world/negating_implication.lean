e9e313e87ce3a04a0c849cd860c17bc4755467b0
Sai Charan Marrivada <saicharan@astrome.co>
2026-05-27 15:55:08 +0530

    remove redundant condition

--- classical_world/negating_implication.lean
+++ classical_world/negating_implication.lean
@@ -1,7 +1,4 @@
 example {P Q : Prop} : ¬ (P → Q) → P ∧ ¬ Q := by
   exact fun h => match Classical.em P with
     | .inl p => ⟨p, fun q => h (fun p => q)⟩
-    | .inr p' => 
-      let p : P := (h (fun p => (p' p).elim)).elim
-      let not_q : ¬Q := (h (fun p => (p' p).elim)).elim
-      ⟨p, not_q⟩
+    | .inr p' => (h (fun p => (p' p).elim)).elim
