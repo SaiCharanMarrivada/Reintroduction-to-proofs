410d9b7f5a19404fbe4d4c22327e1222709da56e
Sai Charan Marrivada <saicharan@astrome.co>
2026-05-27 15:48:46 +0530

    Remove 'have' for the negating implication

--- classical_world/negating_implication.lean
+++ classical_world/negating_implication.lean
@@ -1,6 +1,5 @@
 example {P Q : Prop} : ¬ (P → Q) → P ∧ ¬ Q := by
-  have lemma_p : P ∨ ¬P := Classical.em P
-  exact fun h => match lemma_p with
+  exact fun h => match Classical.em P with
     | .inl p => ⟨p, fun q => h (fun p => q)⟩
     | .inr p' => 
       let p : P := (h (fun p => (p' p).elim)).elim
