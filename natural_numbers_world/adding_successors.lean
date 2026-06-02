c104a94404af7c43204bb23022445d5d3e72e137
Sai Charan Marrivada <saicharan@astrome.co>
2026-06-03 01:44:44 +0530

    Revert "Directly use destructuring syntax for equality"
    This reverts commit 73ef9da160266f66d420c29c02fb67dbc7516f9c.

--- natural_numbers_world/adding_successors.lean
+++ natural_numbers_world/adding_successors.lean
@@ -1,13 +1,8 @@
 open Nat
 example : ∀ m n : Nat, m + succ n = succ (m + n) ∧ succ m + n = succ (m + n) := by
   exact fun m n => ⟨
-    let add_succ (m n : Nat) : m + succ n = succ (m + n) := rfl; add_succ m n,
-    let rec succ_add (m n : Nat) : succ m + n = succ (m + n) :=
-      match n with
+    rfl,
+    by induction n with
       | zero => rfl
-      | succ n' => by rw [add_succ, (succ_add m n')]; rfl;
-    succ_add m n
-    /- by induction n with -/
-    /-   | zero => rfl -/
-    /-   | succ n' ih => rw [add_succ, ih]; rfl -/
+      | succ n' ih => rw [add_succ, ih]; rfl
   ⟩
--- natural_numbers_world/succ_injective.lean
+++ natural_numbers_world/succ_injective.lean
@@ -1,4 +1,5 @@
 open Nat
-def Nat.succ_injective (m n : Nat) : succ m = succ n → m = n := by
-  -- brackets are important here, as we are doing case analysis on the equality
-  exact fun (rfl) => rfl
+theorem Nat.succ_injective : ∀ m n : Nat, succ m = succ n → m = n := by
+  intro m n h 
+  cases h
+  rfl
