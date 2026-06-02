69f1bb8fe2325def4f544ea328f837f8c5e9f778
Sai Charan Marrivada <saicharan@astrome.co>
2026-06-03 01:47:52 +0530

    Destructure equality instead of using tactics.

--- natural_numbers_world/succ_injective.lean
+++ natural_numbers_world/succ_injective.lean
@@ -1,5 +1,4 @@
 open Nat
 theorem Nat.succ_injective : ∀ m n : Nat, succ m = succ n → m = n := by
-  intro m n h 
-  cases h
-  rfl
+  -- brackets are important here, as we are doing case analysis on the equality
+  exact fun m n (rfl) => rfl
