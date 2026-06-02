d62460c4e355b69528ce953320ed7dd3dfe21fe8
Sai Charan Marrivada <saicharan@astrome.co>
2026-06-03 01:56:49 +0530

    Remove impossible cases

--- advanced_functional_world/injectivity.lean
+++ advanced_functional_world/injectivity.lean
@@ -1,6 +1,5 @@
 theorem Bool.not_injective : ∀ x y : Bool, not x = not y → x = y := by
+  -- other 2 cases simply don't occur
   exact fun x y h => match x, y with
     | false, false => rfl
-    | false, true => by cases h
-    | true, false => by cases h
     | true, true => rfl
