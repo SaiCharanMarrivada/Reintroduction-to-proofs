124f726f9b66491cdf8ddc1233eb344c73a12040
Sai Charan Marrivada <saicharan@astrome.co>
2026-06-02 23:41:57 +0530

    Use more direct destructuring syntax instead of pattern matching

--- empty_world/coproduct_with_empty.lean
+++ empty_world/coproduct_with_empty.lean
@@ -1,7 +1,2 @@
 example {A : Type} : (A → A ⊕ Empty) × (A ⊕ Empty → A) := by
-  exact (
-    fun a => Sum.inl a,
-    fun ae => match ae with
-      | Sum.inl a => a
-      | Sum.inr e => e.elim
-  )
+  exact (fun a => Sum.inl a, fun (Sum.inl a) => a)
