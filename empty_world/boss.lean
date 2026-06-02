eb0bb49e78b3549dcccf65c4900de49224407af9
Sai Charan Marrivada <saicharan@astrome.co>
2026-06-02 23:50:44 +0530

    Use destructuring syntax instead of match which handles False branch.

--- empty_world/boss.lean
+++ empty_world/boss.lean
@@ -1,7 +1,5 @@
 example : (Empty ⊕ (Empty → Empty) → (Empty → Empty) × (Empty → Empty)) × ((Empty → Empty) × (Empty → Empty) → Empty ⊕ (Empty → Empty)) := by
   exact ⟨
-    fun f => match f with
-      | Sum.inl e => e.elim
-      | Sum.inr etoe => (etoe, etoe),
+    fun (Sum.inr etoe) => (etoe, etoe), 
     fun ⟨etoe, _⟩ => Sum.inr etoe
   ⟩
