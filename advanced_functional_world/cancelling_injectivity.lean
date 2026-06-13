2d15c0c3c032718ea2916d1d840419de55548755
Sai Charan Marrivada <saicharan@astrome.co>
2026-06-14 01:00:13 +0530

    Replace rw and apply tactics with term-mode proof.

--- advanced_functional_world/cancelling_injectivity.lean
+++ advanced_functional_world/cancelling_injectivity.lean
@@ -1,7 +1,4 @@
 theorem Function.Injective.of_comp {A B C : Type} (f : A → B) (g : B → C) (gf_is_inj : ∀ x y : A, (g ∘ f) x = (g ∘ f) y → x = y) : ∀ x y : A, f x = f y → x = y := by
-  exact fun x y fx_eq_fy => by
+  exact fun x y fx_eq_fy =>
     let h := gf_is_inj x y
-    let h1 : (g ∘ f) x = g (f x) := rfl
-    rw [h1, fx_eq_fy] at h
-    apply h
-    rfl
+    h (congrArg g fx_eq_fy)
