ec089cc14b7c47de8e12983e6b082fe5672611e8
Sai Charan Marrivada <saicharan@astrome.co>
2026-06-15 00:14:49 +0530

    Replace rw tactic with 'congrFun' and 'symm'

--- advanced_functional_world/boss.lean
+++ advanced_functional_world/boss.lean
@@ -1,7 +1,6 @@
 theorem Function.Lawvere_fixed_point {A B : Type} (f : A → A → B) : (∀ g : A → B, ∃ a : A, f a = g) → (∀ n : B → B, ∃ b : B, n b = b) := by
-  intro hsurj n
-  let g : A → B := fun x => n (f x x)
-  let ⟨a, ha⟩ := hsurj g
-  have h1 : n (f a a) = g a := rfl
-  rw [← ha] at h1
-  exact ⟨f a a, h1⟩
+  exact fun hsurj n => 
+    let g : A → B := fun x => n (f x x)
+    let ⟨a, ha⟩ := hsurj g
+    let h1 : n (f a a) = f a a := (congrFun ha a).symm
+    ⟨f a a, h1⟩
