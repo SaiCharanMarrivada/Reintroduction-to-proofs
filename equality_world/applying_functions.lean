763b9ce788775a9f85f91ac7475a58dfe2215e6d
Sai Charan Marrivada <saicharan@astrome.co>
2026-06-17 00:23:55 +0530

    Simplify proofs involving refl.

--- equality_world/applying_functions.lean
+++ equality_world/applying_functions.lean
@@ -1,2 +1,2 @@
 example {A B : Type} {x y : A} (f : A → B) (p : x = y) : f x = f y := by
-  exact (fun f (rfl) => rfl) f p
+  exact (fun (rfl) => rfl) p
--- equality_world/transitivity.lean
+++ equality_world/transitivity.lean
@@ -1,2 +1,2 @@
 example {A : Type} {x y z : A} (p : x = y) (q : y = z) : x = z := by
-  exact (fun (rfl) (rfl) => rfl) p q
+  exact (fun (rfl) => q) p
