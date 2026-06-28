31dc0c5e3a23579c220e3dadcb43b86c8a4d0cb2
ap24110010971 <saigeethikamarrivada359@gmail.com>
2026-06-29 00:49:19 +0530

    Revert "Replace tactic proof with term-mode proof."
    This reverts commit d102b0c0fb760a47db45cb371478f83f25c0353f.

--- advanced_functional_world/invertible_implies_bijective.lean
+++ advanced_functional_world/invertible_implies_bijective.lean
@@ -1,10 +1,16 @@
 theorem Function.bijective_of_invertible' {A B : Type} (f : A → B) (inv : ∃ g : B → A, (∀ a : A, g (f a) = a) ∧ (∀ b : B, f (g b) = b)) : (∀ x y : A, f x = f y → x = y) ∧ (∀ b : B, ∃ x : A, f x = b) := by
-  let ⟨g, left_inv, right_inv⟩ := inv
-  exact ⟨
-    fun x y h =>
-      let h2 := (congrArg g h)
-      let h3 := (left_inv x).symm
-      let h4 := (left_inv y)
-      h3.trans (h2.trans h4),
-    fun b => ⟨g b, right_inv b⟩
-  ⟩
+  constructor
+  intro x y
+  let ⟨g, ⟨hl, _⟩⟩ := inv
+  let b := f x
+  intro fx_eq_fy
+  let s := hl x
+  rw [fx_eq_fy] at s
+  let s2 := hl y
+  rw [s] at s2
+  exact s2
+  
+  intro b
+  let ⟨g, ⟨l, r⟩⟩ := inv
+  let h := r b
+  exact ⟨g b, h⟩
