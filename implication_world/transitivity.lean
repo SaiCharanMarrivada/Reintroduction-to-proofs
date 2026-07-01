596a677a85131e3c3b151707d1662cacd15d6a1d
SaiCharanMarrivada <saicharanmarrivada0@gmail.com>
2026-07-02 02:03:16 +0530

    Replace induction tactic with explicit match

--- implication_world/transitivity.lean
+++ implication_world/transitivity.lean
@@ -1,2 +1,2 @@
 example {P Q R : Prop} : (P → Q) → (Q → R) → (P → R) := by  
-  exact (fun p_to_q q_to_r p => q_to_r (p_to_q p))
+  exact fun p_to_q q_to_r => Function.comp q_to_r p_to_q
--- natural_numbers_world/induction.lean
+++ natural_numbers_world/induction.lean
@@ -1,9 +1,13 @@
+open Nat
+
 -- every natural number is even or odd
 example : ∀ n : Nat , (∃ m : Nat, n = 2 * m) ∨ (∃ m : Nat, n = 2 * m + 1) := by
-  exact fun n => by
-    induction n with
-      | zero => exact .inl ⟨0, rfl⟩
-      | succ n' ih => exact match ih with
-        -- adding 'try' because on older versions of lean, without rfl, goal is not closed
-        | .inl ⟨m1, h1⟩ => .inr ⟨m1, by rw [h1]; try rfl⟩ 
-        | .inr ⟨m2, h2⟩ => .inl ⟨m2 + 1, by rw [h2]; rfl⟩
+  exact (let rec odd_even (n : Nat) : (∃ m : Nat, n = 2 * m) ∨ (∃ m : Nat, n = 2 * m + 1) := 
+    match n with
+      | zero => .inl ⟨0, rfl⟩
+      | succ n' => 
+          match (odd_even n') with
+          | .inl ⟨m1, h1⟩ => .inr ⟨m1, congrArg succ h1⟩ 
+          | .inr ⟨m2, h2⟩ => .inl ⟨m2 + 1, congrArg succ h2⟩;
+    odd_even
+  )
