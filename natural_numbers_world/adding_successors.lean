cebc3b1081b75a96b1470d4cb84c09a7bf1755f6
SaiCharanMarrivada <saicharanmarrivada0@gmail.com>
2026-07-02 03:06:06 +0530

    Replace induction with explicit match

--- natural_numbers_world/adding_successors.lean
+++ natural_numbers_world/adding_successors.lean
@@ -2,7 +2,9 @@ open Nat
 example : ∀ m n : Nat, m + succ n = succ (m + n) ∧ succ m + n = succ (m + n) := by
   exact fun m n => ⟨
     rfl,
-    by induction n with
+    let rec succ_add m n : succ m + n = succ (m + n)  :=
+      match n with
       | zero => rfl
-      | succ n' ih => rw [add_succ, ih]; rfl
+      | succ n' => congrArg succ (succ_add m n'); 
+    succ_add m n
   ⟩
