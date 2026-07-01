30a72475e39a124388d40455575bba2367c2e9a9
SaiCharanMarrivada <saicharanmarrivada0@gmail.com>
2026-07-01 22:42:32 +0530

    Replace tactic proof with term mode proof

--- advanced_functional_world/bijective_implies_invertible.lean
+++ advanced_functional_world/bijective_implies_invertible.lean
@@ -1,10 +1,5 @@
 theorem Function.invertible_of_bijective {A B : Type} (f : A → B) (inj : ∀ x y : A, f x = f y → x = y) (surj : ∀ b : B, ∃ x : A, f x = b) : ∃ g : B → A, (∀ a : A, g (f a) = a) ∧ (∀ b : B, f (g b) = b) := by
-  constructor
-  intro b
-  exact (surj b).choose
-  constructor
-  intro a
-  apply inj
-  exact (surj (f a)).choose_spec
-  intro b
-  exact (surj b).choose_spec
+  exact ⟨
+    fun b => (surj b).choose,
+    ⟨fun a => inj _ _ (surj (f a)).choose_spec, fun b => (surj b).choose_spec⟩
+  ⟩
