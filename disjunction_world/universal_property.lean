8a74d06fe01722804ed007adb87a8e8854e68159
SaiCharanMarrivada <saicharanmarrivada0@gmail.com>
2026-07-02 01:37:23 +0530

    Replace rcases with explicit match

--- disjunction_world/universal_property.lean
+++ disjunction_world/universal_property.lean
@@ -4,8 +4,8 @@ example {P Q R : Prop} : (P ∨ Q → R) ↔ (P → R) ∧ (Q → R) := by
       fun p => h (Or.inl p),
       fun q => h (Or.inr q)
     ⟩,
-    fun ⟨ptor, qtor⟩ porq => by
-      rcases porq with (p | q)
-      . exact (ptor p)
-      . exact (qtor q)
+    fun ⟨ptor, qtor⟩ porq =>
+      match porq with
+      | .inl p => ptor p
+      | .inr q => qtor q
   ⟩
