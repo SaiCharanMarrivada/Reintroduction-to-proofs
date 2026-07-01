34a029162f7c2f955cad4e72769bf786a20e2885
SaiCharanMarrivada <saicharanmarrivada0@gmail.com>
2026-07-02 04:15:21 +0530

    Replace rcases with an explicit match

--- disjunction_world/distributivity.lean
+++ disjunction_world/distributivity.lean
@@ -1,11 +1,11 @@
 example {P Q R : Prop} : P ∧ (Q ∨ R) ↔ (P ∧ Q) ∨ (P ∧ R) := by
   exact ⟨
-    fun ⟨p, qorr⟩ => by
-      rcases qorr with (q | r)
-      . exact Or.inl ⟨p, q⟩
-      . exact Or.inr ⟨p, r⟩,
-    fun pq_or_qr => by
-      rcases pq_or_qr with (⟨p, q⟩ | ⟨p, r⟩)
-      . exact ⟨p, Or.inl q⟩
-      . exact ⟨p, Or.inr r⟩
+    fun ⟨p, qorr⟩ =>
+      match qorr with
+      | .inl q => .inl ⟨p, q⟩ 
+      | .inr r => .inr ⟨p, r⟩,
+    fun pq_or_qr =>
+      match pq_or_qr with
+      | .inl ⟨p, q⟩ => ⟨p, .inl q⟩
+      | .inr ⟨p, r⟩ => ⟨p, .inr r⟩
   ⟩
