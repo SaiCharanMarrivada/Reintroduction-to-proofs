4631d1eb58b18a05c02e14e69e9be839bee38413
SaiCharanMarrivada <saicharanmarrivada0@gmail.com>
2026-07-02 11:50:39 +0530

    Use Or.elim instead of match

--- disjunction_world/using_or.lean
+++ disjunction_world/using_or.lean
@@ -1,4 +1,2 @@
 example {P Q : Prop} : P ∨ Q → Q ∨ P := by
-  exact fun porq => match porq with
-    | Or.inl p => Or.inr p
-    | Or.inr q => Or.inl q
+  exact fun porq => porq.elim (fun p => .inr p) (fun q => .inl q)
