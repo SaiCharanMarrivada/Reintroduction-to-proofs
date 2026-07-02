b6142aac06ecd7eb01523f7f3fcee80db9bdbe80
SaiCharanMarrivada <saicharanmarrivada0@gmail.com>
2026-07-02 12:00:10 +0530

    Replace rcases with explicit match

--- coproduct_world/associativity.lean
+++ coproduct_world/associativity.lean
@@ -1,14 +1,13 @@
 example {A B C : Type} : (((A ⊕ B) ⊕ C) → (A ⊕ (B ⊕ C))) × ((A ⊕ (B ⊕ C)) → ((A ⊕ B) ⊕ C)) := by
-  constructor
-  -- first element
-  intro ab_c
-  rcases ab_c with ((a | b) | c)
-  left; assumption
-  right; left; assumption
-  right; right; assumption
-  -- second element
-  intro a_bc
-  rcases a_bc with (a | (b | c))
-  left; left; assumption
-  left; right; assumption
-  right; assumption
+  exact (
+    fun ab_c => 
+      match ab_c with
+      | .inl (.inl a) => .inl a
+      | .inl (.inr b) => .inr (.inl b)
+      | .inr c => .inr (.inr c),
+    fun a_bc => 
+      match a_bc with
+      | .inl a => .inl (.inl a)
+      | .inr (.inl b) => .inl (.inr b)
+      | .inr (.inr c) => .inr c
+  )
