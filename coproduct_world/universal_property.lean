3a06d324afdbe29bedbe1873ff9a34b0f5f5f9c1
SaiCharanMarrivada <saicharanmarrivada0@gmail.com>
2026-07-02 11:53:41 +0530

    Use Sum.elim instead of match

--- coproduct_world/universal_property.lean
+++ coproduct_world/universal_property.lean
@@ -1,4 +1,2 @@
 example {A B C : Type} (g : A → C) (h : B → C) : (A ⊕ B → C) := by
-  exact fun sum_ab => match sum_ab with
-    | Sum.inl a => g a
-    | Sum.inr b => h b
+  exact fun sum_ab => sum_ab.elim (fun a => g a) (fun b => h b)
