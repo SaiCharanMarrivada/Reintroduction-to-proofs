f5e53a5c2a8004ebdfc3d922d0c2b5f646b6202f
SaiCharanMarrivada <saicharanmarrivada0@gmail.com>
2026-07-02 14:28:07 +0530

    Simplify pattern-matching

--- disjunction_world/associativity.lean
+++ disjunction_world/associativity.lean
@@ -2,16 +2,12 @@ example {P Q R : Prop} : (P ∨ Q) ∨ R ↔ P ∨ Q ∨ R := by
   exact ⟨
     fun porq_orr =>
       match porq_orr with
-      | .inl porq =>
-        match porq with
-        | .inl p => .inl p
-        | .inr q => (.inr ∘ .inl) q
+      | .inl (.inl p) => .inl p
+      | .inl (.inr q) => (.inr ∘ .inl) q
       | .inr r => (.inr ∘ .inr) r,
     fun porqorr =>
       match porqorr with
       | .inl p => (.inl ∘ .inl) p
-      | .inr qorr =>
-        match qorr with
-        | .inl q => (.inl ∘ .inr) q
-        | .inr r => .inr r
+      | .inr (.inl q) => (.inl ∘ .inr) q
+      | .inr (.inr r) => .inr r
   ⟩
