c3d53a7e23ccc6b3dc3595cfb85cef07ade062f2
SaiCharanMarrivada <saicharanmarrivada0@gmail.com>
2026-07-02 11:47:23 +0530

    Replace rcases with explicit match

--- disjunction_world/more_distributivity.lean
+++ disjunction_world/more_distributivity.lean
@@ -1,18 +1,21 @@
 example {P Q R S : Prop} : (P ∨ Q) ∧ (R ∨ S) ↔ (P ∧ R) ∨ (P ∧ S) ∨ (Q ∧ R) ∨ (Q ∧ S) := by
-  exact ⟨
-    fun f => by
-      let ⟨porq, rors⟩ := f
-      rcases porq with (p | q)
-      . rcases rors with (r | s)
-        . exact Or.inl ⟨p, r⟩
-        . exact Or.inr (Or.inl ⟨p, s⟩)
-      . rcases rors with (r | s)
-        . exact Or.inr (Or.inr (Or.inl ⟨q, r⟩))
-        . exact Or.inr (Or.inr (Or.inr ⟨q, s⟩)),
-    fun f => by
-      rcases f with (⟨p, r⟩ | ⟨p, s⟩ | ⟨q, r⟩ | ⟨q, s⟩)
-      . exact ⟨Or.inl p, Or.inl r⟩
-      . exact ⟨Or.inl p, Or.inr s⟩
-      . exact ⟨Or.inr q, Or.inl r⟩
-      . exact ⟨Or.inr q, Or.inr s⟩
-  ⟩ 
+exact ⟨
+  fun ⟨porq, rors⟩ => 
+    match porq with
+    | .inl p =>
+      match rors with
+      | .inl r => .inl ⟨p, r⟩
+      | .inr s => .inr (.inl ⟨p, s⟩)
+    | .inr q =>
+      match rors with
+      | .inl r => .inr (.inr (.inl ⟨q, r⟩))
+      | .inr s => .inr (.inr (.inr ⟨q, s⟩)),
+  fun f => 
+    match f with
+    | .inl ⟨p, r⟩ => ⟨.inl p, .inl r⟩
+    | .inr ps_qr_qs =>
+      match ps_qr_qs with
+      | .inl ⟨p, s⟩ => ⟨.inl p, .inr s⟩
+      | .inr qr_qs => 
+        qr_qs.elim (fun ⟨q, r⟩ => ⟨.inr q, .inl r⟩) (fun ⟨q, s⟩ => ⟨.inr q, .inr s⟩)
+⟩ 
