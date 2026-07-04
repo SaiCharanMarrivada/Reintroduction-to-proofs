d8cdcd3ab425e31aabed61d754b62b028ebf6d33
SaiCharanMarrivada <saicharanmarrivada0@gmail.com>
2026-07-04 23:36:56 +0530

    Fix a typo.

--- equivalence_world/coproduct_symmetry.lean
+++ equivalence_world/coproduct_symmetry.lean
@@ -3,10 +3,12 @@ example {A B : Type} : (A ⊕ B) ≃ (B ⊕ A) := by
   exact ⟨
     Sum.swap,
     Sum.swap,
-    fun aorb => match aorb with
+    fun aorb => 
+      match aorb with
       | .inl a => rfl
       | .inr b => rfl,
-    fun bora => match bora with
+    fun bora =>
+      match bora with
       | .inl b => rfl
-      | .inr b => rfl
+      | .inr a => rfl
   ⟩
